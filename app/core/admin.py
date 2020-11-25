5b22af2d2ffb2e1fa20ffd9817f3f026aafe8aeb
Scott Kirschner <scott.kirschner@perficient.com>
2020-11-25 12:56:44 -0500

    updated admin fields for user

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,15 +7,16 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['created_at']
     list_display = ['email', 'first_name', 'last_name', 'created_at']
+    readonly_fields = ('id', 'uuid', 'created_at', 'updated_at',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('first_name', 'last_name', 'user_name')}),
+        (_('Personal Info'), {'fields': ('id', 'uuid', 'first_name', 'last_name', 'user_name')}),
         (_('Level'), {'fields': ('level',)}),
         (
             _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'), {'fields': ('last_login',)}),
+        (_('Important dates'), {'fields': ('created_at', 'updated_at', 'last_login',)}),
     )
     add_fieldsets = (
         (None, {
@@ -23,6 +24,5 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
-    readonly_fields = ('id', 'uuid', 'created_at', 'updated_at')
 
 admin.site.register(models.User, UserAdmin)
