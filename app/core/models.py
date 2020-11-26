03ac01591c60ae8c50f5e4135ed90f06b28bdce0
scott <scottakirschner@gmail.com>
2020-11-26 12:04:55 -0500

    added achievement model

--- app/core/models.py
+++ app/core/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -26,6 +27,7 @@ class UserManager(BaseUserManager):
         
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
@@ -42,3 +44,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Achievement(models.Model):
+    """Achievements associated with points"""
+    achievement = models.CharField(max_length=1024)
+    points = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.achievement
+
+class StudentAchievement(models.Model):
+    """Associates achievements with students"""
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    achievement = models.ForeignKey(
+        Achievement,
+        on_delete=models.CASCADE,    
+    )
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.achievement.achievement
+
